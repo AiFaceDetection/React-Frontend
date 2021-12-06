ab512828768455f80c7b83eb671f7f87db7c60d6
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2021-12-06 17:16:52 +0700

    fix window

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from face_recognition.face_recognition_cli import image_files_in_folder
 
 import numpy as np
 from itertools import chain
+from imageio import imread
 
 
 app = Flask(__name__)
@@ -55,6 +56,7 @@ def api():
                 result = data['data']
                 b = bytes(result, 'utf-8')
                 image = b[b.find(b'/9'):]
+                img = imread(io.BytesIO(base64.b64decode(image)))
                 im = Image.open(io.BytesIO(base64.b64decode(image)))
                 im.save(directory + '/face.jpg')
 
@@ -82,11 +84,11 @@ def api():
                 img1 = img1.crop(card_area)
                 img2 = img2.crop(face_area)
 
-                img1.save(os.path.join(card_dir , 'card.jpg'))
-                img2.save(os.path.join(face_dir , 'face.jpg'))
+                img1.save('./card/card.jpg')
+                img2.save('./face/face.jpg')
 
-                full_file_path = os.path.join(face_dir , 'face.jpg')
-                predictions = predict(full_file_path, model_path="trained_knn_model.clf")
+                full_file_path = './face/face.jpg'
+                predictions = predict(full_file_path, img, model_path="trained_knn_model.clf")
                 names = []
                 try:
                     for name, (top, right, bottom, left) in predictions:
@@ -118,39 +120,39 @@ def apii():
     return result
 
 
-def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.48):
+def predict(X_img_path,img , knn_clf=None, model_path=None, distance_threshold=0.48):
 
-    if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
-        raise Exception("Invalid image path: {}".format(X_img_path))
+    # if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
+    #     raise Exception("Invalid image path: {}".format(X_img_path))
 
-    if knn_clf is None and model_path is None:
-        raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
+    # if knn_clf is None and model_path is None:
+    #     raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
 
     # Load a trained KNN model (if one was passed in)
     if knn_clf is None:
         with open(model_path, 'rb') as f:
             knn_clf = pickle.load(f)
 
-    try:
-        # Load image file and find face locations
-        X_img = face_recognition.load_image_file(X_img_path)
-        X_face_locations = face_recognition.face_locations(X_img)
 
-        # If no faces are found in the image, return an empty result.
-        if len(X_face_locations) == 0:
-            return []
+    # Load image file and find face locations
+    X_img = face_recognition.load_image_file(X_img_path)
+    # X_img = img
+    X_face_locations = face_recognition.face_locations(X_img)
 
-        # Find encodings for faces in the test iamge
-        faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+    # If no faces are found in the image, return an empty result.
+    if len(X_face_locations) == 0:
+        return []
 
-        # Use the KNN model to find the best matches for the test face
-        closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
-        are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+    # Find encodings for faces in the test iamge
+    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
 
-        # Predict classes and remove classifications that aren't within the threshold
-        return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
-    except:
-        pass
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
 
 
 if __name__ == '__main__':
